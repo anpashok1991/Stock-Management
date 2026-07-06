5e513df36452cc20c4a932b14919be485b2ac478
Ashok Gupta <anp.ashok1991@gmail.com>
2026-07-06 15:39:30 +0530

    fix all issues

--- src/StockManagement.Application/Features/Products/Commands/DeleteProductCommand.cs
+++ src/StockManagement.Application/Features/Products/Commands/DeleteProductCommand.cs
@@ -1,7 +1,9 @@
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using StockManagement.Application.Common.Interfaces;
 using StockManagement.Application.Common.Models;
 using StockManagement.Domain.Entities;
+using StockManagement.Domain.Entities.Inventory;
 
 namespace StockManagement.Application.Features.Products.Commands;
 
@@ -20,6 +22,13 @@ internal class DeleteProductCommandHandler : IRequestHandler<DeleteProductComman
     {
         var product = await _context.Products.FindAsync(new object[] { request.Id }, ct)
             ?? throw new Common.Exceptions.NotFoundException(nameof(Product), request.Id);
+
+        var stockItems = await _context.StockItems
+            .Where(s => s.ProductId == request.Id)
+            .ToListAsync(ct);
+
+        if (stockItems.Any())
+            _context.StockItems.RemoveRange(stockItems);
 
         product.IsDeleted = true;
         product.ModifiedAt = DateTime.UtcNow;
